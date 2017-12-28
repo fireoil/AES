e95f96e57342d58514d5b7961d26d708e0c9811a
zhang xin <zhangxinpaper@126.com>
2017-12-28 22:36:45 +0800

    complete decrypt edit

--- decrypt.c
+++ decrypt.c
@@ -1,1 +1,148 @@
 #include "decrypt.h"
+int decrypt_date(char *in, char *out, char *decrypt_key)
+{
+    int array[4][4];
+    int key_length = strlen(decrypt_key);
+    int i,j;
+    char *w;
+    const int block_num = 4;
+    int k_num;
+    int round_num;
+    switch(key_length)
+    {
+    case 16:
+        k_num = 4;
+        round_num = 10;
+        break;
+    case 24:
+        k_num = 6;
+        round_num = 12;
+        break;
+    case 32:
+        k_num = 8;
+        round_num = 14;
+        break;
+    default:
+        break;
+    }
+
+    w = (char *)malloc(block_num*(round_num+1)*4);
+    extend_key(decrypt_key, w);
+
+    int wArray[4][4];
+    for (i=0; i<strlen(in); i++)
+    {
+        convert_to_int_array(in+i, array);
+        add_round_key(array, round_num);
+        for(j=round_num-1; j>=1; j--)
+        {
+            de_sub_bytes(array);
+            de_shift_rows(array);
+            de_mix_columns(array);
+            get_array_from_4W(j, wArray, w);
+            de_mix_columns(wArray);
+            add_round_two_array(array, wArray);
+        }
+        de_sub_bytes(array);
+        de_shift_rows(array);
+        add_round_key(array, 0, w);
+        convert_array_to_string(array, out);
+    }
+
+    free(w);
+    return 0;
+}
+static int get_num_from_SINV_BOX(int index)
+{
+    int row = getLeft4Bit(index);
+    int column = getRight4Bit(index);
+    return S_INV[row][column];
+}
+static void de_sub_bytes(int array[4][4])
+{
+    int i,j;
+    for (i=0; i<4; i++)
+        for (j=0; j<4; j++)
+        {
+            array[i][j] = get_num_from_SINV_BOX(array[i][j]);
+        }
+}
+
+static void right_loop_4int(int array[4], int step)
+{
+    int temp[4];
+    int i, index;
+    for (i=0; i<4; i++)
+    {
+        temp[i] = array[i];
+    }
+    index = step % 4 == 0 ? 0:step%4;
+    index = 3 - index;
+    for (i=3; i>=0; i--)
+    {
+        array[i] = temp[index];
+        index--;
+        index = index == -1?3 : index;
+    }
+}
+
+static void de_shift_rows(int array[4][4])
+{
+    int row_two[4], row_three[4], row_four[4];
+    int i;
+    for (i=0; i<4; i++)
+    {
+        row_two[i] = array[1][i];
+        row_three[i] = array[2][i];
+        row_four[i] = array[3][i];
+    }
+    right_loop_4int(row_two, 1);
+    right_loop_4int(row_three, 2);
+    right_loop_4int(row_four, 3);
+    for(i=0; i<4; i++)
+    {
+        array[1][i] = row_two[i];
+        array[2][i] = row_three[i];
+        array[3][i] = row_four[i];
+    }
+}
+static void de_mix_columns(int array[4][4])
+{
+    int temp[4][4];
+    int i,j;
+    for (i=0; i<4; i++)
+        for (j=0; j<4; j++)
+            temp[i][j] = array[i][j];
+    for (i=0; i<4; i++)
+        for (j=0; j<4; j++)
+        {
+            array[i][j] = GF_mul(de_col_mix[i][0], temp[0][j]) ^ GF_mul(de_col_mix[i][1], temp[1][j]) ^ GF_mul(de_col_mix[i][2], temp[2][j]) ^ GF_mul(de_col_mix[i][3], temp[3][j]);
+        }
+}
+
+static void add_round_two_array(int arrayA[4][4], int arrayB[4][4])
+{
+    int i,j;
+    for ( i=0; i<4; i++ )
+        for ( j=0; j<4; j++ )
+            arrayA[i][j] ^= arrayB[i][j];
+}
+
+static void get_array_from_4W(int i, int array[4][4], char *w)
+{
+    int index, j;
+    int col_one[4], col_two[4], col_three[4], col_four[4];
+    index = i*4;
+    splitIntToArray(w[index], col_one);
+    splitIntToArray(w[index+1], col_two);
+    splitIntToArray(w[index+2], col_three);
+    splitIntToArray(w[index+3], col_four);
+
+    for (j=0; j<4; j++)
+    {
+        array[j][0] = col_one[j];
+        array[j][1] = col_two[j];
+        array[j][2] = col_three[j];
+        array[j][3] = col_four[j];
+    }
+}
--- utility.c
+++ utility.c
@@ -14,3 +14,229 @@ void splitIntToArray(int num, int array[4])
     array[2] = three & 0x000000ff;
     array[3] = num & 0x000000ff;
 }
+
+int GF_mul(int n, int s)
+{
+    int result;
+    switch(n)
+    {
+    case 1:
+        result = s;
+        break;
+    case 2:
+        result = GF_mul12(s);
+        break;
+    case 3:
+        result = GF_mul13(s);
+        break;
+    case 4:
+        result = GF_mul4(s);
+        break;
+    case 0x9:
+        result = GF_mul9(s);
+        break;
+    case 0xb:
+        result = GF_mul11(s);
+        break;
+    case 0xd:
+        result = GF_mul13(s);
+        break;
+    case 0xe:
+        result = GF_mul14(s);
+        break;
+    default:
+        break;
+    }
+
+    return result;
+}
+
+int GF_mul2(int s)
+{
+    int result = s << 1;
+    int a7 = result & 0x00000100;
+
+    if (a7 != 0)
+    {
+        result = result & 0x000000ff;
+        result = result ^ 0x1b;
+    }
+    return result;
+}
+
+int GF_mul3(int s)
+{
+    return  GF_mul2(s)^s;
+}
+
+int GF_mul4(int s)
+{
+    return GF_mul3(s)^s;
+}
+
+int GF_mul8(int s)
+{
+    return GF_mul4(GF_mul4(s));
+}
+
+int GF_mul9(int s)
+{
+    return GF_mul8(s)^s;
+}
+
+int GF_mul11(int s)
+{
+    return GF_mul9(GF_mul2(s));
+}
+
+int GF_mul12(int s)
+{
+    return GF_mul11(s) ^ s;
+}
+
+int GF_mul13(int s)
+{
+    return GF_mul12(s)^s;
+}
+
+int GF_mul14(int s)
+{
+    return GF_mul13(s) ^ s;
+}
+
+/**
+ * 获取整形数据的低8位的左4个位
+ */
+int getLeft4Bit(int num)
+{
+    int left = num & 0x000000f0;
+    return left >> 4;
+}
+
+/**
+ * 获取整形数据的低8位的右4个位
+ */
+int getRight4Bit(int num)
+{
+    return num & 0x0000000f;
+}
+
+/** \brief   extend the key
+ *  \param   the string representation of key
+ */
+void extend_key(char *key, char *w)
+{
+    int i;
+    int j;
+    for (i=0; i<4; i++)
+        w[i] = get_word_from_str(key[i*4]);
+    for (i=4,j=0; i<strlen(w); i++)
+    {
+        if (i%4 == 0)
+        {
+            w[i] = w[i-4] ^ T(w[i-1], j);
+            j++;
+        }
+        else
+        {
+            w[i] = w[i-4] ^ w[i-1];
+        }
+    }
+}
+
+int get_word_from_str(char *str)
+{
+    int one = get_int_from_char(str[0]);
+    int two = get_int_from_char(str[1]);
+    int three = get_int_from_char(str[2]);
+    int four = get_int_from_char(str[3]);
+
+    return one | two | three | four;
+}
+
+
+/**
+ * 密钥扩展中的T函数
+ */
+int T(int num, int round)
+{
+    int numArray[4];
+    int i;
+    splitIntToArray(num, numArray);
+    leftLoop4int(numArray, 1);              // 字循环
+
+    // 字节代换
+    for(i = 0; i < 4; i++)
+        numArray[i] = getNumFromSBox(numArray[i]);
+
+    int result = mergeArrayToInt(numArray);
+    return result ^ Rcon[round];
+}
+
+int get_int_from_char(char c)
+{
+    int result = (int)c;
+    return result & 0x000000FF;
+}
+
+/**
+ * 将数组中的元素循环左移step位
+ */
+void leftLoop4int(int array[4], int step)
+{
+    int temp[4];
+    int i;
+    for(i = 0; i < 4; i++)
+        temp[i] = array[i];
+
+    int index = step % 4 == 0 ? 0 : step % 4;
+    for(i = 0; i < 4; i++)
+    {
+        array[i] = temp[index];
+        index++;
+        index = index % 4;
+    }
+}
+
+/**
+ * 根据索引，从S盒中获得元素
+ */
+int getNumFromSBox(int index)
+{
+    int row = getLeft4Bit(index);
+    int col = getRight4Bit(index);
+    return S[row][col];
+}
+
+/**
+ * 把数组中的第一、二、三和四元素分别作为
+ * 4字节整型的第一、二、三和四字节，合并成一个4字节整型
+ */
+int mergeArrayToInt(int array[4])
+{
+    int one = array[0] << 24;
+    int two = array[1] << 16;
+    int three = array[2] << 8;
+    int four = array[3];
+    return one | two | three | four;
+}
+
+/** \brief      convert string to int array
+ *
+ * \param       string
+ * \param       int array
+ */
+void convert_to_int_array(char *str, int array[4][4])
+{
+    int k = 0;
+    int i, j;
+    for (i=0; i<4; i++)
+    {
+        for (j=0; j<4; j++)
+        {
+            array[j][i] = get_int_from_char(str[k]);
+            k++;
+        }
+    }
+}
+
