24ba5463018631fd00161ca6159520802dcf1882
zhang xin <zhangxinpaper@126.com>
2017-12-30 20:19:33 +0800

    start debug

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #include "encrypt.h"
 #include "decrypt.h"
@@ -8,9 +9,10 @@
 // I used many chinese language in code, sorry!
 // Because I am a chinese. I love my country.
 
-#define     NAME_LENGTH         65535
+#define     NAME_LENGTH         1024
+#define     PATH_LENGTH         1024
 
-int read_key(char *keyfile, int *round_num, uint8_t *key, uint8_t *s_box, uint8_t *inv_s_box);
+int read_key(char *keyfile, char *key);
 static int get_file_length(FILE *fp);
 
 /** \brief      主函数，根据输入的命令来加密或解密数据
@@ -24,9 +26,16 @@ int main(int argc, char *argv[])
 {
     FILE *fp_r = NULL;
     FILE *fp_w = NULL;
+    char input[NAME_LENGTH] = "";
     char result[NAME_LENGTH] = "";
     char deal_string[8] = "";
     int c = 0;
+    char path_name[PATH_LENGTH];
+    getcwd(path_name, sizeof(path_name));
+
+    argc = 3;
+    argv[1] = "encrypt";
+    argv[2] = "E:\\projects\\CB_projects\\AES_DIY\\bin\\Debug\\hello";
 
     if (argc == 1)
     {
@@ -40,17 +49,30 @@ int main(int argc, char *argv[])
     }
     else if (argc == 3)
     {
+        strcpy(input, argv[2]);
         strcat(result, argv[2]);
         strcat(result, ".result");
     }
     else if (argc == 4)
     {
+        strcpy(input, argv[2]);
         strcpy(result, argv[3]);
     }
 
+    strcat(path_name, "\\");
+    strcat(path_name, input);
+
+    if ((fp_r = fopen(argv[2], "r")) == NULL)
+        goto end;
+    if ((fp_w = fopen(result, "w")) == NULL)
+        goto end;
+
     strcpy(deal_string, argv[1]);
-    if (fp_r = fopen(argv[2], "r")) goto end;
-    if (fp_w = fopen(result, "w")) goto end;
+
+    // get the string of input file
+    int len_r = get_file_length(fp_r);
+    // test the function of get_file_length
+    printf("the file length is %d.\n", len_r);
 
     if (strcmp(deal_string, "encrypt") == 0)
     {
@@ -72,9 +94,6 @@ int main(int argc, char *argv[])
         printf("e.g.   AES    encrypt     file_name    result\n");
         printf("e.g.   AES    decrypt     file_name    result\n");
     }
-
-    // 每一步的结果都要测试，当然是直接设置好argv[]
-
 
 end:
     fclose(fp_r);
